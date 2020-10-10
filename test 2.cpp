627812da9b117bb6f63a42b56f4528d4dec6bb4b
Kanti Lal Bunkar <56160251+Kanti2001@users.noreply.github.com>
2020-10-10 19:03:35 +0530

    Add files via upload

---
+++ test 2.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int a,b;
+	cout <<"\nEnter Value of a:";
+	cin >>a;
+	
+	cout <<"\nEnter Value of b:";
+	cin >>b;
+	
+	cout <<"\nSum:"<<a+b;
+	return 0;
+	
+}

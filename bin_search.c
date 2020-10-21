5e7cc0127ef0804c17c71fc2bf288d48fab89ecf
Rajat009-Ra1 <73220391+Rajat009-Ra1@users.noreply.github.com>
2020-10-21 14:10:03 +0530

    Create bin_search.c

---
+++ bin_search.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int bin_search (int x[] ,int size,int key)
+int main()
+{
+	int a[100],n,key,i,bin_search;
+	printf("enter the size of the array:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("enter the element of a[%d]=%d",i,a[i]);
+		scanf("%d",a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	  printf("enter the element of a[%d]=%d",i,a[i]);
+	  scanf("%d",a[i]);
+	}
+	printf("enter the key for which that you wants to know in the array:");
+	scanf("%d",key);
+	bin_search=(int a[i],int size ,int key);
+	
+}
+int bin_search (int x[] ,int size,int key)
+{
+	int i,f,mid;
+	i=0;f=n-1;
+	mid=(i+f)/2;
+	if(mid>i)
+	{
+	}
+}
